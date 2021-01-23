64c3afa15e674b9e2dace50dcec4f2c9672b718c
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-23 13:07:22 -0300

    initial component

---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Alert from './Alert';
+
+const App = () => {
+    return (
+      <div className="container mt-5">
+        <Alert/>
+        </div>
+    )
+  }
+
+export default App;
--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 ReactDOM.render(
-  <h1>Hello world!</h1>,
+  <App/> ,
   document.getElementById('root')
 );
