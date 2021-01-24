7fb900113ab631753f34ed8009b5b73ce7d406fe
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-24 14:20:38 -0300

    integração back-end part I

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () =>{
+
+    useEffect( () =>{
+        fetch('http://localhost:3000/api/1.0/product')
+        .then(response => response.json())
+        .then(response => console.log(response));
+    },[]);
+
     return(
         <div className="catalog-container">
             <h1 className="catalog-title">Catálogo de produtos</h1>
@@ -11,7 +18,7 @@ const Catalog = () =>{
                 <Link to="/products/1"><ProductCard /></Link>
             </div>
         </div>
-    )
+    );
 }
 
 export default Catalog;
