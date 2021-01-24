516fbde617dd7ec8988b0492f629bb2201e9588f
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-24 10:05:15 -0300

    product card custom

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () =>{
@@ -6,7 +7,7 @@ const Catalog = () =>{
         <div className="catalog-container">
             <h1 className="catalog-title">Catálogo de produtos</h1>
             <div className="catalog-products">
-
+                <ProductCard />
             </div>
         </div>
     )
