98b904e5993903fb4730ded5da5ae2d0063e1c70
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-24 16:53:06 -0300

    loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/ProductsResponse';
 import makeRequest from '../../core/utils/reques';
 import ProductCard from './components/ProductCard';
-import ProductCardLoader from './components/ProductCardLoader';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () =>{
