e7f6ea1b1d5fe5ed027aaa984182f0f5f149d406
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-04-02 22:34:29 -0300

    03-05 Criando barra de navegação do Admin

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './styles.scss';
 const Admin = () =>{
     return (
         <div>
-            <h1>Admin xxxxxxxxxxxxxxxxxxxxx</h1>
+            <Navbar/>
         </div>
     )
 }
