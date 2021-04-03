6d598a3bec61b1e6a91c053dfbb7e82cc7cede9b
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-04-02 21:13:57 -0300

    03-03 Renderizando os items da paginação dinâmicamente Concluída

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -4,22 +4,24 @@ import { ProductsResponse } from 'core/types/ProductsResponse';
 import makeRequest from 'core/utils/reques';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
+import Pagination from 'core/components/Pagination';
 import './styles.scss';
 
 const Catalog = () =>{
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect( () =>{
         const params={
-            page:0,
+            page:activePage,
             linesPerPage:12
         }
         setLoading(true);
         makeRequest({url:'/api/1.0/products', params})
         .then(response => setProductsResponse(response.data))
         .finally(() => setLoading(false));
-    },[]);
+    },[activePage]);
 
     return(
         <div className="catalog-container">
@@ -27,6 +29,7 @@ const Catalog = () =>{
             <div className="catalog-products">
                 {isLoading ? <ProductCardLoader amount={3}/>:( productsResponse?.content.map(product => (<Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product} /></Link>)))}
             </div>
+            {productsResponse && <Pagination totalPages={productsResponse.totalPages} activePage={activePage} onChange={page => setActivePage(page)}></Pagination> }
         </div>
     );
 }
